67f3c15aa09bc2a1ff8bfe7f507c2ff90c6c08cc
unknown <gmarti37@156TR4160MP-04.campus.MCGILL.CA>
2013-09-25 20:47:34 -0400

    Updated lab 1 files

--- Lab 1/Part1Main.c
+++ Lab 1/Part1Main.c
@@ -1,7 +1,7 @@
 	#include <stdio.h>
 	#include "STM32F4xx.h" 
 
-	extern void encryptionAsm(unsigned int *key,  char *data);
+	extern void encryptionAsm(unsigned int *key,  char *data, int delta);
 
 	int __main(int argc, char* argv[]) {
 //		printf("test\n");
@@ -9,7 +9,8 @@
 		//int y = 2;
 		//int z = x + y;
 		unsigned int key_temp[4];
-		char data_temp[8];
+		char *data_temp;
+		int delta = 0x9E3779B9;
 		
 		//key_temp = { 0xBEEF, 0xDEAD, 0xCAFE, 0xFADE };
 		int i = 0;
@@ -17,10 +18,17 @@
 			key_temp[i] = 0xBEEF;
 		}
 		
-		for (i = 0; i < 4; i++) {
-			data_temp[i] = 0x00;
-		}
+		data_temp = "TESTTEST";
 		
-		encryptionAsm(key_temp, data_temp);
+	//	for (i = 0; i < 4; i++) {
+	//		data_temp[i] = 0x00;
+	//	}
+		
+
+		
+		encryptionAsm(key_temp, data_temp, delta);
+		
+		printf("data_temp: %s", data_temp);
+		
 		return 0;
 	}
--- Lab 1/lab1Asm.s
+++ Lab 1/lab1Asm.s
@@ -6,23 +6,35 @@
 	; REGISTERS
 	; R0: Pointer to key -> D0
 	; R1: Pointer to data -> D1
-	; R2: Sum
-	; R3: Delta
+	; R2: Delta
+	; R3: Sum
 	; R4: Loop counter (N)
 	; R5: Temp
 	; R6-R8: T0-T3
 	; R9-R12: K0-K3
 	
-encryptionAsm	
+encryptionAsm		
 	
-	PUSH {R0} ; Get pointer to key from stack
-	PUSH {R1} ; Get pointer to data from stack
+	PUSH {R0} ; Put key pointer on stack
+	PUSH {R1} ; Put data pointer on stack
+	PUSH {R2} ; Put delta on stack
 	
 	MOV R5, R1 ; Copy data pointer to temp register
 	
-	LDM R0, {R9-R12} ; Load key into registers 9 - 12
+	LDM R0, {R9 - R12} ; Load key into registers 9 - 12
 	
 	LDM R5, {R0 - R1} ; Load data into D0 and D1
+	
+	MOV R4, #0 ; Set loop counter to 0
+	
+	MOV R0, #0xCAFE
+	
+	STM R5, {R0 - R1} ; Put data back in memory
+	
+	;POP {R2}
+	;POP {R1}
+	;POP {R0}
+	; Increment stack pointer by 3 registers
 	
 	BX LR
 
