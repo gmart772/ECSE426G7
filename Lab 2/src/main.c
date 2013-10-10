3ca8029332a7fb1ab0b0fb5818e2e136a8c8e1d3
unknown <gmarti37@156TR4160MP-11.campus.MCGILL.CA>
2013-10-10 15:18:32 -0400

    Updated lab 2

--- Lab 2/src/main.c
+++ Lab 2/src/main.c
@@ -1,19 +1,63 @@
 #include <stdio.h>
 #include "stm32f4xx.h"
+
+#include "stm32f4xx_adc.h"
 #include "stm32f4xx_conf.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_rcc.h"
 
 //#include "gpio_example.h"
 
 
+
 int main()
 {
-
-	//GPIO_example_config();
+	GPIO_InitTypeDef gpio_init_s;
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+	GPIO_StructInit(&gpio_init_s);
+	gpio_init_s.GPIO_Pin = GPIO_Pin_12;
+	gpio_init_s.GPIO_Mode = GPIO_Mode_OUT;
+	gpio_init_s.GPIO_Speed = GPIO_Speed_100MHz;
+	gpio_init_s.GPIO_OType = GPIO_OType_PP;
+	gpio_init_s.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOD, &gpio_init_s);
 	
-	while(1) {
+	ADC_InitTypeDef adc_init_s;
+	ADC_CommonInitTypeDef adc_common_init_s;
+	
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
+	adc_common_init_s.ADC_Mode = ADC_Mode_Independent;
+	adc_common_init_s.ADC_Prescaler = ADC_Prescaler_Div2;
+	adc_common_init_s.ADC_DMAAccessMode = ADC_DMAAccessMode_Disabled;
+	adc_common_init_s.ADC_TwoSamplingDelay = ADC_TwoSamplingDelay_5Cycles;
+	ADC_CommonInit(&adc_common_init_s);
+	
+	adc_init_s.ADC_Resolution = ADC_Resolution_12b;
+	adc_init_s.ADC_ScanConvMode = DISABLE;
+	adc_init_s.ADC_ContinuousConvMode = DISABLE;
+	adc_init_s.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
+	adc_init_s.ADC_DataAlign = ADC_DataAlign_Right;
+	adc_init_s.ADC_NbrOfConversion = 1;
+	ADC_Init(ADC1, &adc_init_s);
+	
+	ADC_Cmd(ADC1, ENABLE);
 
+	ADC_TempSensorVrefintCmd(ENABLE);
+	ADC_RegularChannelConfig(ADC1, ADC_Channel_TempSensor, 1, ADC_SampleTime_28Cycles);
+	
+	ADC_ContinuousModeCmd(ADC1, ENABLE);
+
+	ADC_SoftwareStartConv(ADC1);
+
+	uint16_t voltage;
+	int i = 0;
+	while(i < 1000) {
+	
+		// Temperature (in °C) = {(VSENSE – V25) / Avg_Slope} + 25
+		voltage = ADC_GetConversionValue(ADC1);
 		//GPIO_ToggleBits(GPIOD, GPIO_Pin_12);
 		//printf("This is a TEST?");
+		i++;
 		
 	}
 }
